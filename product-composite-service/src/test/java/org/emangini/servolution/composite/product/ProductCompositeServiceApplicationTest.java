776063aa1b6e9e9fef25cf3632bd7beaf810383b
Ed and Gravy <me@emangini.com>
2024-12-22 00:57:29 -0500

    Added build.gradle.kts and core applications + contextLoad Test for recommendation and product composite

---
+++ product-composite-service/src/test/java/org/emangini/servolution/composite/product/ProductCompositeServiceApplicationTest.java
@@ -0,0 +1,11 @@
+package org.emangini.servolution.composite.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCompositeServiceApplicationTest {
+
+    @Test
+    void contextLoads() { }
+}
---
+++ recommendation-service/src/test/java/org/emangini/servolution/core/recommendation/RecommendationServiceApplicationTest.java
@@ -0,0 +1,12 @@
+package org.emangini.servolution.core.recommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationServiceApplicationTest {
+
+    @Test
+    void contextLoads() { }
+
+}
