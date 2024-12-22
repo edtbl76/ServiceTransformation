0545969fc714ccf60b5a5d6d1a0da7dcf83a98a8
Ed and Gravy <me@emangini.com>
2024-12-22 00:33:33 -0500

    Added build.gradle.kts and core applications + contextLoad Test for product service

---
+++ product-service/src/test/java/org/emangini/servolution/core/product/ProductServiceApplicationTest.java
@@ -0,0 +1,11 @@
+package org.emangini.servolution.core.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTest {
+
+    @Test
+    void contextLoads() { }
+}
