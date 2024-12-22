64d567a3d42c1edd953e8095bc97b5d74ef300dc
Ed and Gravy <me@emangini.com>
2024-12-22 00:46:28 -0500

    Added build.gradle.kts and core applications + contextLoad Test for review service

---
+++ review-service/src/test/java/org/emangini/servolution/core/review/ReviewServiceApplicationTest.java
@@ -0,0 +1,12 @@
+package org.emangini.servolution.core.review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewServiceApplicationTest {
+
+    @Test
+    void contextLoads() { }
+
+}
