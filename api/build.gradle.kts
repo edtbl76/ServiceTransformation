1d2ef4d60e66d86304e7ebdfa1c8c2556642c740
Ed and Gravy <me@emangini.com>
2024-12-24 15:09:18 -0500

    refactoring: multi-project build - added kotlin-dsl - added allprojects block for plugins and dependencies - removed superfluous directives from child projects

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,25 +1,8 @@
-plugins {
-    id("java")
-    id("io.spring.dependency-management") version  "1.1.7"
-}
-
-repositories {
-    mavenCentral()
-}
-
 dependencies {
     implementation(platform("org.springframework.boot:spring-boot-dependencies:3.4.1"))
     implementation("org.springdoc:springdoc-openapi-starter-common:2.7.0")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
-    compileOnly("org.projectlombok:lombok:1.18.34")
-    annotationProcessor("org.projectlombok:lombok:1.18.34")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
 }
 
 tasks.register("prepareKotlinBuildScriptModel") {}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,42 @@
 plugins {
+    kotlin("jvm") version "2.1.0"
     id("java")
+    id("io.spring.dependency-management") version  "1.1.7"
+    base
 }
 
-subprojects {
+allprojects {
     group = "org.emangini.servolution"
 
     apply {
         plugin("java")
         plugin("idea")
+        plugin("io.spring.dependency-management")
+        plugin("org.jetbrains.kotlin.jvm")
     }
 
     java {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        implementation("org.springframework.boot:spring-boot-starter-actuator")
+        implementation("org.springframework.boot:spring-boot-starter-webflux")
+        compileOnly("org.projectlombok:lombok:1.18.36")
+        annotationProcessor("org.projectlombok:lombok:1.18.36")
+        testImplementation(platform("org.junit:junit-bom:5.10.0"))
+        testImplementation("org.junit.jupiter:junit-jupiter")
+    }
+
+    tasks.test {
+        useJUnitPlatform()
+    }
 }
 
 
-repositories {
-    mavenCentral()
-}
 
-dependencies {
-    compileOnly("org.projectlombok:lombok:1.18.36")
-    annotationProcessor("org.projectlombok:lombok:1:18.36")
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
